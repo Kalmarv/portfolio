8a654f702c4b799c4a0a3fcb7262581659b5c07b
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-08-07 21:46:25 -0600

    ✨ Add icons and more animations

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,13 +1,27 @@
 import { useAtom } from 'jotai'
-import { useEffect, useRef } from 'react'
+import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 import { useTrail, config, a, useSpring } from 'react-spring'
 import { useIntersectionObserver } from 'usehooks-ts'
 import { pageAtom } from '../../utils/store'
+import { useDrag, useHover } from '@use-gesture/react'
 
-const MultipleElements: React.FC<{ open: boolean; delay?: number; children: JSX.Element[] }> = ({
+type animationProps = {
+  open: boolean
+  delay?: number
+  children: JSX.Element[]
+  containerStyle?: string
+  outerStyle?: string
+  innerStyle?: string
+}
+
+const AnimatedChildren: React.FC<animationProps> = ({
   open,
   children,
   delay,
+  containerStyle,
+  outerStyle,
+  innerStyle,
 }) => {
   const trail = useTrail(children.length, {
     config: config.stiff,
@@ -17,13 +31,66 @@ const MultipleElements: React.FC<{ open: boolean; delay?: number; children: JSX.
     delay: open ? delay ?? 0 : 0,
   })
   return (
-    <div className='max-w-sm p-2'>
+    <div className={containerStyle ?? ''}>
       {trail.map(({ ...style }, index) => (
-        <a.div key={index} className='relative w-full' style={style}>
-          <a.div>{children[index]}</a.div>
+        <a.div key={index} className={outerStyle ?? ''} style={style}>
+          <a.div className={innerStyle ?? ''}>{children[index]}</a.div>
         </a.div>
       ))}
     </div>
+  )
+}
+
+const AnimatedIcons: React.FC<animationProps> = ({
+  open,
+  children,
+  delay,
+  containerStyle,
+  outerStyle,
+  innerStyle,
+}) => {
+  const [trail, api] = useTrail(
+    children.length,
+    () => ({
+      config: config.stiff,
+      opacity: open ? 1 : 0,
+      y: open ? 0 : 40,
+      from: { opacity: 0, y: 40 },
+      delay: open ? delay ?? 0 : 0,
+    }),
+    [open]
+  )
+
+  return (
+    <div className={containerStyle ?? ''}>
+      {trail.map(({ ...style }, index) => (
+        <a.div key={index} className={outerStyle ?? ''} style={style}>
+          <a.div className={innerStyle ?? ''}>{children[index]}</a.div>
+        </a.div>
+      ))}
+    </div>
+  )
+}
+
+const Icon: React.FC<{ icon: string }> = ({ icon }) => {
+  const [props, api] = useSpring(
+    () => ({
+      transform: 'rotateY(0deg)',
+    }),
+    []
+  )
+
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <a.img
+      onMouseEnter={() => api.start({ transform: 'rotateY(360deg)' })}
+      onMouseLeave={() => api.start({ transform: 'rotateY(0deg)' })}
+      style={{ ...props, transformStyle: 'preserve-3d' }}
+      alt={icon}
+      src={`/icons/${icon}.svg`}
+      width={40}
+      height={40}
+    />
   )
 }
 
@@ -32,6 +99,24 @@ const About = () => {
   const ref = useRef<HTMLDivElement | null>(null)
   const entry = useIntersectionObserver(ref, { threshold: 0.75 })
   const isVisible = !!entry?.isIntersecting
+  const icons = [
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'Python',
+    'Nextjs',
+    'React',
+    'Svelte',
+    'Express',
+    'Tailwind',
+    'Postgres',
+    'Git',
+    'Node',
+    'NPM',
+    'Docker',
+    'VSCode',
+  ]
 
   useEffect(() => {
     if (isVisible) setPage(1)
@@ -40,7 +125,10 @@ const About = () => {
   return (
     <>
       <div className='flex flex-col justify-center h-screen place-items-center' ref={ref}>
-        <MultipleElements open={isVisible}>
+        <AnimatedChildren
+          open={isVisible}
+          containerStyle='max-w-sm p-2'
+          outerStyle='relative w-full'>
           <h1 className='font-bold text-2xl'>About Me</h1>
           <div className='p-2'></div>
           <section>
@@ -68,10 +156,16 @@ const About = () => {
             </p>
           </section>
           <div className='p-2'></div>
-          <h1 className='font-bold text-2xl'>Technology</h1>
+          <h1 className='font-bold text-2xl'>Tech Stack</h1>
           <div className='p-2'></div>
           <section>Here&apos;s some of the technologies I use and love the most.</section>
-        </MultipleElements>
+        </AnimatedChildren>
+        <div className='p-4'></div>
+        <AnimatedIcons open={isVisible} containerStyle='grid grid-cols-4 gap-6' delay={1000}>
+          {icons.map((icon) => (
+            <Icon key={icon} icon={icon} />
+          ))}
+        </AnimatedIcons>
       </div>
     </>
   )
