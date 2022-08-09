d3e8f7dd5bf9afde6c1359fb9060fd2156a5ca28
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-08-08 21:36:33 -0600

    💄 Logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,20 @@
+const Logo = () => {
+  return (
+    <svg viewBox='205.1 165.5 130 130' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        height='100%'
+        width='100%'
+        fillOpacity='1'
+        strokeWidth='5'
+        strokeLinejoin='round'
+        strokeDasharray='none'
+        strokeOpacity='1'
+        paintOrder='fill'
+        d='M43.1 7.6c-29.4 15.9-26.7 40-26.7 40v85S43 119 43 92.6zM55.1 85l20-20 45 45h-40ZM53.2 72.6l65-65h-35l-30 30z'
+        transform='translate(201.9 160.5)'
+      />
+    </svg>
+  )
+}
+
+export default Logo
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { useAtom } from 'jotai'
 import type { NextPage } from 'next'
 import { useRef } from 'react'
+import Logo from '../components/logo'
 import About from '../components/sections/about'
 import Landing from '../components/sections/landing'
 import { pageAtom } from '../utils/store'
@@ -26,6 +27,11 @@ const Home: NextPage = () => {
     <>
       <div className='fixed top-0 left-0 z-50'>
         <div className='flex p-4 font-bold gap-4'>
+          <a
+            className='transition w-6 h-6 p-1 bg-branding-dark rounded hover:bg-branding-primary stroke-branding-primary fill-branding-primary hover:stroke-branding-dark hover:fill-branding-dark'
+            href='https://portfolio.kalmarv.xyz/'>
+            <Logo />
+          </a>
           <a className={linkStyle(page, 0)} onClick={() => scroll(0)}>
             Home
           </a>
